6fc9b4e0cce7274fb6b0fdcdd57c9990625d24c4
meraj2704 <mh1669101@gmail.com>
2024-08-14 00:06:31 +0600

    projct update

---
+++ src/constant/carouselData.ts
@@ -0,0 +1,16 @@
+export const carouselData = [
+    {
+        name:"Food Shop",
+        image:"/assets/food-shop.png",
+        link:"https://food-shop-app-client.vercel.app/"
+    },
+    {
+        name:"Law Firm",
+        image:"/assets/law-firm.png",
+        link:"https://lawfirm-main.vercel.app/"
+    },{
+        name:"Health Care",
+        image:"/assets/health-care.png",
+        link:"https://health-care-client-phi.vercel.app/"
+    }
+]
