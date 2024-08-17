8fd540bd92cbae2fe0e8180417c76bb3b65310f4
meraj2704 <mh1669101@gmail.com>
2024-08-17 23:02:16 +0600

    new project update

--- src/constant/carouselData.ts
+++ src/constant/carouselData.ts
@@ -1,16 +1,41 @@
 export const carouselData = [
-    {
-        name:"Food Shop",
-        image:"/assets/food-shop.png",
-        link:"https://food-shop-app-client.vercel.app/"
-    },
-    {
-        name:"Law Firm",
-        image:"/assets/law-firm.png",
-        link:"https://lawfirm-main.vercel.app/"
-    },{
-        name:"Health Care",
-        image:"/assets/health-care.png",
-        link:"https://health-care-client-phi.vercel.app/"
-    }
-]
+  {
+    name: "Law Firm",
+    image: "/assets/law-firm.png",
+    link: "https://lawfirm-main.vercel.app/",
+  },
+  {
+    name:"Portfolio-Client",
+    image:"/images/client-portfolio.png",
+    link: "https://client-laniding-page-figma.vercel.app/",
+  },{
+    name:"POS Page",
+    image: "/assets/pos-page.png",
+    link: "https://uw-pos-page.vercel.app/",
+  },
+  {
+    name:"Restaurant",
+    image: "/assets/restaurant.png",
+    link: "https://restaurant-lemon-pi.vercel.app/",
+  },
+  {
+    name:"GPT3",
+    image: "/assets/gpt3.png",
+    link: "https://gpt3-client.vercel.app/",
+  },
+  {
+    name:"Modern Bank",
+    image: "/assets/modern-bank.png",
+    link: "https://hoobank-eight-kappa.vercel.app/",
+  },
+  {
+    name: "Health Care",
+    image: "/assets/health-care.png",
+    link: "https://health-care-client-phi.vercel.app/",
+  },
+  {
+    name: "Food Shop",
+    image: "/assets/food-shop.png",
+    link: "https://food-shop-app-client.vercel.app/",
+  },
+];
